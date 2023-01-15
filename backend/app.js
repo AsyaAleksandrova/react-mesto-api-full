5e4e24bb4b346949e9ca63f736498befac243b36
Asya Aleksandrova <mail.aleksandrova@yandex.ru>
2023-01-15 21:58:38 +0300

    Change to 3000 port

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const validateAuth = require('./middlewares/validateAuth');
 const errorHandler = require('./middlewares/errorhandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors({
